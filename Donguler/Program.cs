77330aea9fdfda0787219e01c5172a7f45518ea6
Emre Kuranoğlu <kuranoglue@gmail.com>
2021-01-18 20:45:11 +0300

    16.01.2021 Ders sonu

---
+++ Donguler/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirme Kampı";
+            string kurs2 = "Programlamaya Başlangıç İçin Temel Kurs";
+            string kurs3 = "Java";
+
+            string[] kurslar = new string[] { "Yazılım Geliştirme Kampı", "Programlamaya Başlangıç İçin Temel Kurs", "Java", "Python", "C#" };
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+
+            Console.WriteLine("For bitti");
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            int[] sayilar = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            for(int i=1; i<10; i+=3)
+            {
+                Console.WriteLine(i);
+            }
+
+            double[] kusuratlar = new double[] { 0.1, 0.2, 0.3 };
+
+            for(double a=0.1; a < kusuratlar.Length; a++)
+            {
+                Console.WriteLine(a);
+            }
+
+
+            Console.WriteLine("Sayfa Sonu");
+        }
+    }
+}
---
+++ Odevler/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Odevler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.AnaBaslik = "Kadın";
+            urun1.Kategori = "Giyim";
+            urun1.AltKategori = "Sweatshirt";
+            urun1.UrunAdi = "Mavi Blok Renkli Oversize Sweatshirt";
+
+            Urun urun2 = new Urun();
+            urun2.AnaBaslik = "Erkek";
+            urun2.Kategori = "Ayakkabı";
+            urun2.AltKategori = "Spor Ayakkabı";
+            urun2.UrunAdi = "Siyah Sneaker Spor Ayakkabı";
+
+            Urun urun3 = new Urun();
+            urun3.AnaBaslik = "Çocuk";
+            urun3.Kategori = "Oyuncak";
+            urun3.AltKategori = "Eğitici Oyuncak";
+            urun3.UrunAdi = "Eğlenceli Bambu Çubuklar";
+
+            Urun[] urunler = new Urun[] { urun1, urun2, urun3 };
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.AnaBaslik + " / " + urun.Kategori + " / " + urun.AltKategori + " / " + urun.UrunAdi);
+            }
+
+            for (int i = 0; i < urunler.Length; i++)
+            {
+                Console.WriteLine(urunler[i].AnaBaslik + " / " + urunler[i].Kategori + " / " + urunler[i].AltKategori + " / " + urunler[i].UrunAdi);
+            }
+
+            int b = 0;
+          
+            while (b<=2)
+            {
+                if (b==1)
+                {
+                    Console.WriteLine(urunler);
+                }
+            }
+
+        }
+    }
+}
+
+
+class Urun
+{
+
+    public string AnaBaslik { get; set; }
+
+    public string Kategori { get; set; }
+
+    public string AltKategori { get; set; }
+
+    public string UrunAdi { get; set; }
+
+}
+
+
+
