82369594edab9933100cedd43088af5f1a8d7a8c
Emre Kuranoğlu <kuranoglue@gmail.com>
2021-01-24 15:54:06 +0300

    23.01.2021 ders sonu

---
+++ Oop2/Musteri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    class Musteri
+    {
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+
+        //Bir veri üzerinde sayısal bir işlem yapmıyorsan (toplama ve çıkartma gibi) onlara string değeri vermek daha doğrudur (ör: TC No.).
+        //Bir nesnede bir değeri kullanmak zorunda değil gibi gözüküyorsan, o nesneye ait değiş gibi duruyorsa o alan demek ki orada bir "soyutlama" hatası
+        //var demektir.
+
+    }
+}
